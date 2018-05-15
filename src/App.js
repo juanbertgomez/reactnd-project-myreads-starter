30c98cc76936550307c1f2ca616523117779692b
Unknown <andreagarloz@gmail.com>
2018-05-15 09:27:45 +0200

    book details

--- src/App.js
+++ src/App.js
@@ -10,17 +10,24 @@ class BooksApp extends React.Component {
     books : [
       {
         id:'libro1',
-        name:'To kill a mockingbird',
+        name:'To kill a mockingbir',
+        state:'currentlyReadin',
+        author:'mamaguebo',
+        image:'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+      },
+      {
+        id:'libro2',
+        name:'Ender´s name',
         state:'currentlyReading',
-        author:'Harper Lee',
-        image:''
+        author:'cabroncete',
+        image:'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api'
       }
     ]
 
   }
   render() {
     return (
-      <BookShelf/>
+      <BookShelf books={this.state.books}/>
     )
   }
 }
